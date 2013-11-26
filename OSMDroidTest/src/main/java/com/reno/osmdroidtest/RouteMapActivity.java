e00fadb7a6891fd34f31364779eb4696c250ce88
Renê Lopes <renelopes.over@gmail.com>
2013-11-26 01:56:05 -0200

    Testes com mbtiles map

--- OSMDroidTest/src/main/java/com/reno/osmdroidtest/RouteMapActivity.java
+++ OSMDroidTest/src/main/java/com/reno/osmdroidtest/RouteMapActivity.java
@@ -1,65 +1,97 @@
 package com.reno.osmdroidtest;
 
+import android.graphics.Color;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.modules.IArchiveFile;
+import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.io.File;
 
 public class RouteMapActivity extends ActionBarActivity {
 
+
+    private static final String TAG = RouteMapActivity.class.getName();
+
+    // Most of this is useless
+    private XYTileSource MBTILESRENDER = new XYTileSource(
+            "mbtiles",
+            ResourceProxy.string.offline_mode,
+            0, 10,  // zoom min/max <- should be taken from metadata if available
+            256, ".png", "http://i.dont.care.org/");
+
+    private MapView mOsmv;
+    private ResourceProxy mResourceProxy;
+    private MapTileProviderArray mProvider;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route_map);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        mapBeginConfig();
+        loadUI();
+        mapEndConfig();
+
     }
 
+    private void mapBeginConfig(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.route_map, menu);
-        return true;
+        String packageDir = "/com.reno.osmdroidtest";
+        mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+        SimpleRegisterReceiver simpleReceiver = new SimpleRegisterReceiver(this);
+
+        File f = new File(Environment.getExternalStorageDirectory() + packageDir, "brasil.mbtiles");
+
+        IArchiveFile[] files = { MBTilesFileArchive.getDatabaseFileArchive(f) };
+        MapTileModuleProviderBase moduleProvider = new MapTileFileArchiveProvider(simpleReceiver, MBTILESRENDER, files);
+
+        mProvider = new MapTileProviderArray(MBTILESRENDER, null,
+                new MapTileModuleProviderBase[]{ moduleProvider }
+        );
+
+        this.mOsmv = new MapView(this, 256, mResourceProxy, mProvider);
+
+        this.mOsmv.setBackgroundColor(Color.rgb(181,208,208));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void mapEndConfig(){
+        this.mOsmv.setBuiltInZoomControls(true);
+        this.mOsmv.setMultiTouchControls(true);
+
+        mOsmv.getController().setZoom(4);
+        mOsmv.setMinZoomLevel(4);
+        mOsmv.setMaxZoomLevel(8);
+        double lon = -49.636;
+        double lat = -15.008;
+
+        IGeoPoint point = new GeoPoint(lat, lon); // lat lon and not inverse
+        mOsmv.getController().setCenter(point);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    private void loadUI() {
+        this.setContentView(R.layout.activity_route_map);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.maplayout);
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_route_map, container, false);
-            return rootView;
-        }
+        this.mOsmv.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        ));
+        rl.addView(this.mOsmv);
     }
 
 }
